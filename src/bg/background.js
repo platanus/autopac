e6d37cb3f36effd35245b4573614cec71be32e1d
joaquin <joaquin.jjb@gmail.com>
2017-12-27 11:57:03 -0300

    Selective actions for baks URL s

--- src/bg/background.js
+++ src/bg/background.js
@@ -5,6 +5,25 @@
 // });
 
 
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // Match pages
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlMatches: '(bancoestado.cl|bancochile.cl)' },
+          })
+        ],
+        // Activate exetnsion actions
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
+
+
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -11,8 +11,14 @@ function getPage(){
         tabTitle = tabs[0].title;
         console.log("Title");
         console.log(tabURL);
-        document.getElementById("text-holder").innerHTML = tabURL;
+
+        var name = tabURL.split(/\.(.+)/)[1];;
+        document.getElementById("text-holder").innerHTML = name.split(/\.com|\.cl/)[0];
     });
+}
+
+function getName(url){
+    
 }
 
 document.addEventListener('DOMContentLoaded', function() {
