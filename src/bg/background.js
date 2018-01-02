a8044399bf22a16f76b1ce2ece07f09a6d5e3b6d
joaquin <joaquin.jjb@gmail.com>
2018-01-02 14:41:49 -0300

    refactor + styles

--- src/bg/background.js
+++ src/bg/background.js
@@ -72,7 +72,6 @@ chrome.tabs.onUpdated.addListener(function() {
 });
 
 
-
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
