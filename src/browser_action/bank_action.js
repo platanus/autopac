484f653b196acc507bff3c31c5b8b0b7162767d4
joaquin <joaquin.jjb@gmail.com>
2018-01-05 15:59:40 -0300

    feat(button): Merge 2 buttons in one button+ funtion()

--- src/browser_action/bank_action.js
+++ src/browser_action/bank_action.js
@@ -1,78 +1,79 @@
-//Get the url in the current tab and extract the domain name 
-function getPage(callback){
-    var tabURL;
-    var tabTitle;
-    var name;
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, function(tabs) {
-        tabURL = tabs[0].url;
-        tabTitle = tabs[0].title;
-       $.getJSON('active_pages.json', json => {
-            var array_exp = new Array(); 
-            for (var name in json) {
-                array_exp.push(json[name].domain);                
-            }
-            var regex = new RegExp(array_exp.join("|"));
+//Get the url in the current tab and extract the domain name
+function getPage(callback) {
+  var tabURL;
+  var tabTitle;
+  var name;
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function(tabs) {
+    tabURL = tabs[0].url;
+    tabTitle = tabs[0].title;
+    $.getJSON('active_pages.json', json => {
+      var array_exp = new Array();
+      for (var name in json) {
+        array_exp.push(json[name].domain);
+      }
+      var regex = new RegExp(array_exp.join("|"));
 
-            // Identify URL matches(banks domain) 
-            name = tabURL.match(regex)[0];
-            name = name.split(/.cl/)[0];
-            //console.log(name);
-            if (callback) {
-                callback(name)        
-            }
-        });
+      // Identify URL matches(banks domain)
+      name = tabURL.match(regex)[0];
+      name = name.split(/.cl/)[0];
+      //console.log(name);
+      if (callback) {
+        callback(name)
+      }
     });
+  });
 }
 
 //Open form in the same tab
-function openForm(json){
-    // Open in the same tab the form to fill
-console.log(json.name);
-    if (json.domain=="santander.cl") {    
-        santanderForm();
-    }
-    else {   
-        chrome.tabs.update({ url: json.form_url });
-
-
-    }
-}
-
-// Add lsitener for main button
-document.addEventListener('DOMContentLoaded', function() {
-    var mainButton = document.getElementById('mainButton');
-    // onClick's logic below:
-    mainButton.addEventListener('click', function() {
-        getPage( bank_name => {
-          $.getJSON("active_pages.json", json => {
-                document.getElementById("text-holder").innerHTML = "<p>"+ json[bank_name].name  +"</p>" + "<p>"+ json[bank_name].domain  +"</p>";
-                // Open form to fill in the same tab    
-                           
-                openForm(json[bank_name]);
-                
-            });
-        }); 
+function openForm(json) {
+  // Open in the same tab the form to fill
+  console.log(json.name);
+  if (json.domain == "santander.cl") {
+    santanderForm();
+  } else {
+    chrome.tabs.update({
+      url: json.form_url
     });
 
-});
 
+  }
+}
+
+// Add listener for main button
 document.addEventListener('DOMContentLoaded', function() {
-    var fillFormBtn = document.getElementById('fillFormBtn');
-    fillFormBtn.addEventListener('click', fillButton);
+  var mainButton = document.getElementById('mainButton');
+
+  getPage(bank_name => {
+    $.getJSON("active_pages.json", json => {
+      document.getElementById("text-holder").innerHTML += "<p> Origen:" + json[bank_name].name + "</p>";
+
+      // onClick's logic below:
+      mainButton.addEventListener('click', function() {
+        // Open form to fill in the same tab
+        openForm(json[bank_name]);
+
+        //wait until the page is loaded
+        setTimeout(() => {
+          fillButton();
+        }, 3000);
+      });
+    });
+  });
+
 });
 
 function fillButton() {
-    chrome.tabs.executeScript({
-        code: 'location.href="javascript:fill_my_form(); void 0";'
-    });
+  chrome.tabs.executeScript({
+    code: 'location.href="javascript:fill_my_form(); void 0";'
+  });
 }
 
 //Santander dont change its URL, need a fun
 function santanderForm() {
-    chrome.tabs.executeScript({
-        code: 'location.href="javascript:goToSantanderForm(); void 0";'
-    });
+  chrome.tabs.executeScript({
+    code: 'location.href="javascript:goToSantanderForm(); void 0";'
+  });
 }
