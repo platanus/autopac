560d15585c7f401b2f48e5d6119dba7e789d72f2
Tewi <tewisp@gmail.com>
2018-01-08 15:54:25 -0300

    fix(): fix extension option page transferencia data

--- src/options/options.js
+++ src/options/options.js
@@ -1,11 +1,22 @@
-class Transferencia {
+// helper class that syncs the form with the data of the options page
+class TransferenciaOptions {
 
+    // initialize form data from another transferencia 
     constructor(obj){
-        obj && Object.entries(obj).forEach( attr => {
-            this[attr[0]] = attr[1];
-        });
+        if (!obj) return;
+        obj.monto && (this.monto = obj.monto);
+        this.destinatario = {};
+        if (obj.destinatario) {
+            obj.destinatario.rut && (this.rutDestinatario = obj.destinatario.rut);
+        }
+        this.programacion = {};
+        if (obj.programacion) {
+            obj.programacion.frecuencia && (this.frecuencia = obj.programacion.frecuencia);
+            obj.programacion.fechaInicio && (this.fechaInicio = obj.programacion.fechaInicio);
+            obj.programacion.fechaTermino && (this.fechaTermino = obj.programacion.fechaTermino);
+        }
     }
-    
+    // Setters and getter helper functions to get and set data to the form 
     get monto(){
         return document.getElementById("monto").value
     }
@@ -38,35 +49,42 @@ class Transferencia {
         document.getElementById("rutDestinatario").value = val;
     }
 
+    // returns raw data of the object (without setters and gettes) 
     toObject() {
         let obj = {};
         this.monto && (obj.monto = this.monto);
-        this.rutDestinatario && (obj.rutDestinatario = this.rutDestinatario);
-        this.frecuencia && (obj.frecuencia = this.frecuencia);
-        this.fechaInicio && (obj.fechaInicio = this.fechaInicio);
-        this.fechaTermino && (obj.fechaTermino = this.fechaTermino);
+        obj.destinatario = {};
+        this.rutDestinatario && (obj.destinatario.rut = this.rutDestinatario);
+        obj.programacion = {};
+        this.frecuencia && (obj.programacion.frecuencia = this.frecuencia);
+        this.fechaInicio && (obj.programacion.fechaInicio = this.fechaInicio);
+        this.fechaTermino && (obj.programacion.fechaTermino = this.fechaTermino);
 
         return obj;
     }
 }
 
-var transferencia = new Transferencia();
-var t = transferencia;
 
-function saveTransferencia(){
-    chrome.storage.local.set({"transferencia": transferencia.toObject()});
+// object that contains the data of the tranferencia 
+var transferencia = new TransferenciaOptions();
+
+var storage = chrome.storage.local; //alias 
+// saves the transferencia data to the local storage
+function saveTransferenciaToLocalStorage(){
+    storage.set({"transferencia": transferencia.toObject()});
 }
-
-function getTransferencia(){
-    chrome.storage.local.get("transferencia", function(x) {
-        transferencia = new Transferencia(x.transferencia)
+// loads the data from the local storage
+function loadTransferenciaFromLocalStorage(){
+    storage.get("transferencia", (data) => {
+        transferencia = new TransferenciaOptions(data.transferencia)
     });
 }
+// add logic to the option page buttons
+document.getElementById('save').addEventListener('click', saveTransferenciaToLocalStorage);
+document.getElementById('get').addEventListener('click', loadTransferenciaFromLocalStorage);
+loadTransferenciaFromLocalStorage();
 
 
-var storage = chrome.storage.local;
-document.getElementById('save').addEventListener('click', saveTransferencia);
-document.getElementById('get').addEventListener('click', getTransferencia);
-getTransferencia();
+
 
 
